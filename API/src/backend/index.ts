a5e027c84c7b71291da1c134d60a93adfde8122c
furtub2 <vladyslav.ishchuk.2@gmail.com>
2023-03-30 13:55:24 +0200

    Localhost server / express installed

--- API/src/backend/index.ts
+++ API/src/backend/index.ts
@@ -1,1 +1,9 @@
-console.log("Hi");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(3000,() => console.log("Server listening at port 3000"));
