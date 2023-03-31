376a644b25576e022abcd5233ded6481d8d46311
furtub2 <vladyslav.ishchuk.2@gmail.com>
2023-03-31 20:19:16 +0200

    test

--- API/src/backend/index.ts
+++ API/src/backend/index.ts
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.send("Hello World!");
 });
 
 app.listen(3000,() => console.log("Server listening at port 3000"));
