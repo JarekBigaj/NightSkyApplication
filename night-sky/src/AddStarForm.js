d8cd6f850ebef6058c8e92868c8948f7ce788b9a
Jarek Bigaj <j.bigaj@interia.pl>
2023-04-26 21:04:57 +0200

    added night sky properties

--- night-sky/src/AddStarForm.js
+++ night-sky/src/AddStarForm.js
@@ -1,20 +1,72 @@
 import styled from "styled-components";
 import { useState,useEffect } from "react";
 
+const LUNAR_PHASES = [
+    { id: 'full', name: 'Full Moon' },
+    { id: 'new', name: 'New Moon' },
+    { id: 'first-quarter', name: 'First Quarter' },
+    { id: 'third-quarter', name: 'Third Quarter' }
+];
+
 function AddStarForm(){
-    const [range,  setRange] = useState(5);
+    const [nightSkyWeather,  setNightSkyWeather] = useState(() =>{
+        return {
+                cloudinessLevel: '5',
+                lunarPhase: '',
+                fogDensityLevel: '5'
+            }
+    });
 
     const handleCloudinessChange = (event) => {
-        setRange(event.target.value);
+        setNightSkyWeather({
+            ...nightSkyWeather,
+            cloudinessLevel: event.target.value
+          });
     }
+
+    const handlePhaseChange = (event) => {
+        setNightSkyWeather({
+            ...nightSkyWeather,
+            lunarPhase: event.target.value
+        });
+    };
+
+    const handleFogChange = (event) => {
+        setNightSkyWeather({
+            ...nightSkyWeather,
+            fogDensityLevel: event.target.value
+        });
+    };
+
+    console.log(nightSkyWeather);
 
     return (
         <div>
-            <Label value="Zachmurzenie"/>
             <SliderWrapper name="cloudinessLevel">
-                <Slider type="range" min="1" max="10" value={range} onChange={handleCloudinessChange} />
-                <Label value={range}/>
+                <Label value="Cloudiness Level : "/>
+                <Slider type="range" min="1" max="10" value={nightSkyWeather.cloudinessLevel} onChange={handleCloudinessChange} />
+                <Label value={nightSkyWeather.cloudinessLevel}/>
             </SliderWrapper>
+            <CheckBoxWrapper>
+                {LUNAR_PHASES.map((phase) => (
+                    <label key={phase.id}>
+                    <Input
+                        type="radio"
+                        name="lunar-phase"
+                        value={phase.id}
+                        checked={nightSkyWeather.lunarPhase === phase.id}
+                        onChange={handlePhaseChange}
+                    />
+                    {phase.name}
+                    </label>
+                ))}
+            </CheckBoxWrapper>
+            <SliderWrapper name="fogDensityLevel">
+                <Label value="Fog Density Level : "/>
+                <Slider type="range" min="1" max="10" value={nightSkyWeather.fogDensityLevel} onChange={handleFogChange} />
+                <Label value={nightSkyWeather.fogDensityLevel}/>
+            </SliderWrapper>
+            
         </div>
         )
 }
@@ -25,8 +77,20 @@ const Label = styled(({className,value}) => {
 
 `;
 
+const CheckBoxWrapper = styled (({className,children})=>{
+    return <div className={className}>{children}</div>
+})`
+
+`;
+
 const SliderWrapper = styled(({className,name,children})=>{
     return <div name={name} className={className}>{children}</div>
+})`
+
+`;
+
+const Input = styled(({className,type,value,onChange,name})=>{
+    return <input className={className} name={name} type={type} value={value} onChange={onChange}/>
 })`
 
 `;
