75b401e29469710b68a8be28db416690dd51b185
furtub2 <vladyslav.ishchuk.2@gmail.com>
2023-05-11 20:20:54 +0200

    add delete star and constellation

---
+++ API/src/backend/routes/stars/delete.deleteSelectedStar.ts
@@ -0,0 +1,26 @@
+import { RequestHandler } from 'express'
+import { StatusCodes } from 'http-status-codes'
+import { prisma } from '../../database'
+import { checkPrismaError } from '../../utils'
+
+export const DeleteSelectedStar: RequestHandler = async (req,res) => {
+
+    const { id } = req.body
+    try{
+        const DeletedStar = await prisma.star.delete({
+            where: {
+              id
+            }
+          })
+
+        res.status(StatusCodes.OK)
+
+    } catch (err) {
+        console.error(err)
+        const response = checkPrismaError(err, {
+        
+        })
+        res.status(response.status)
+        res.send(response.message)
+        }
+}
