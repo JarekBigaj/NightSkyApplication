a909c142b9344a4f0265cbd9292796c94d1e2f8e
furtub2 <vladyslav.ishchuk.2@gmail.com>
2023-05-11 20:24:56 +0200

    update AllDataStars

--- API/src/backend/routes/stars/get.AllDataStars.ts
+++ API/src/backend/routes/stars/get.AllDataStars.ts
@@ -1,21 +1,22 @@
-import { RequestHandler } from 'express'
+import { RequestHandler } from "express"
 import { StatusCodes } from 'http-status-codes'
 import { prisma } from '../../database'
 import { checkPrismaError } from '../../utils'
 
-export const getAllDataStars: RequestHandler = async (req, res) => {
+export const getAllDataStars: RequestHandler = async (req,res) => {
 
-  try{
-    const AllStars = await prisma.star.findMany()
+    try{
+        const AllStars = await prisma.star.findMany();
 
-    res.status(StatusCodes.OK)
-    res.send({ ...AllStars })
-  } catch (err) {
-    console.error(err)
-    const response = checkPrismaError(err, {
-    
-    })
-    res.status(response.status)
-    res.send(response.message)
-    }
+        res.send(AllStars)
+        res.status(StatusCodes.OK)
+
+    } catch (err) {
+        console.error(err)
+        const response = checkPrismaError(err, {
+        
+        })
+        res.status(response.status)
+        res.send(response.message)
+        }
 }
