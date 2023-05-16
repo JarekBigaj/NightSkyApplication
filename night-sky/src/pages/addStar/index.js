9a8b3fb21d2575fbe77d955f9e3b6849928e9bbe
Jarek Bigaj <j.bigaj@interia.pl>
2023-05-16 17:51:32 +0200

    update form add star

--- night-sky/src/pages/addStar/index.js
+++ night-sky/src/pages/addStar/index.js
@@ -1,17 +1,26 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 const AddStar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [formData, setFormData] = useState({
     name: '',
-    email: '',
-    message: '',
+    description: '',
+    urlImage: '',
+    constellationId:''
   });
 
   const handleFormSubmit = e => {
     e.preventDefault();
-    // Handle form submission logic here
-    console.log(formData);
+    console.log(formData)
+    fetch('http://127.0.0.1:3600/api/stars/addStar',{
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
     setIsOpen(false);
   };
 
@@ -27,8 +36,10 @@ const AddStar = () => {
     setIsOpen(!isOpen);
   };
 
+  
+
   return (
-    <div>
+    <PopupCard>
       <button onClick={togglePopup}>Add Star</button>
       {isOpen && (
         <div className="popup-card">
@@ -38,19 +49,58 @@ const AddStar = () => {
               <input type="text" name="name" value={formData.name} onChange={handleInputChange} />
             </label>
             <label>
-              Email:
-              <input type="email" name="email" value={formData.email} onChange={handleInputChange} />
+              Description:
+              <textarea name="description" value={formData.description} onChange={handleInputChange} />
             </label>
             <label>
-              Message:
-              <textarea name="message" value={formData.message} onChange={handleInputChange} />
+              urlImage:
+              <input type="text" name="urlImage" value={formData.urlImage} onChange={handleInputChange} />
+            </label>
+            <label>
+              Constellation:
+              <input type="text" name="constellationId" value={formData.constellationId} onChange={handleInputChange} />
             </label>
             <button type="submit">Submit</button>
           </form>
         </div>
       )}
-    </div>
+    </PopupCard>
   );
 };
 
+const PopupCard = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 400px;
+  padding: 20px;
+  background-color: #ffffff;
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
+  z-index: 9999;
+
+  form {
+    display: grid;
+    grid-gap: 10px;
+  }
+
+  label {
+    display: block;
+    font-weight: bold;
+  }
+
+  input,
+  textarea {
+    width: 100%;
+    padding: 5px;
+  }
+
+  button {
+    display: block;
+    margin-top: 10px;
+  }
+`;
+
+
 export default AddStar;
+
