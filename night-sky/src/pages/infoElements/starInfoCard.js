8a8e0842bdc80a5ab3e35614e91ab9aa29f9a4b2
Jarek Bigaj <j.bigaj@interia.pl>
2023-05-17 20:37:37 +0200

    get data from selected star

--- night-sky/src/pages/infoElements/starInfoCard.js
+++ night-sky/src/pages/infoElements/starInfoCard.js
@@ -1,6 +1,7 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+
+const API_GET_STAR_DATA = `http://127.0.0.1:3600/api/stars/getDataSelectedStar`
 
 const StarInfoCard = styled(({className}) => {
     const [currentStarData,setCurrentStateData] = useState();
@@ -8,13 +9,30 @@ const StarInfoCard = styled(({className}) => {
     const querryString = window.location.search;
     const urlParams = new URLSearchParams(querryString);
     const getStarId = urlParams.get('id');
+
     console.log(getStarId)
 
+    const getData = async () =>{
+        const response = await fetch(`${API_GET_STAR_DATA}?id=${getStarId}`);
+        if(!response.ok) throw new Error(`This is an HTTP error: The status is ${response.status}`);
+        const json = await response.json();
+        return json;
+    }
 
-    useEffect(()=>{
+    console.log(`${API_GET_STAR_DATA}?id=${getStarId}`)
 
+    useEffect(() => {
+        (async () =>{
+            try{
+                const response = await getData();
+                setCurrentStateData(response);
+            } catch (error) {
+                console.log(error);
+            }
+        })()
     },[])
 
+    console.log(currentStarData)
     return (
         <span>123</span>
     )
