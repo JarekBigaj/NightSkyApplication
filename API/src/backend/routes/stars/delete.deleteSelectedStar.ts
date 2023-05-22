de242d5a7929814ec4ae5aec4a9e5527678359e4
furtub2 <vladyslav.ishchuk.2@gmail.com>
2023-05-22 20:38:26 +0200

    correct delete Star

--- API/src/backend/routes/stars/delete.deleteSelectedStar.ts
+++ API/src/backend/routes/stars/delete.deleteSelectedStar.ts
@@ -2,17 +2,32 @@ import { RequestHandler } from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { prisma } from '../../database'
 import { checkPrismaError } from '../../utils/prisma.utils'
+import { ValidateID } from '../../utils/validation.utils'
 
 export const DeleteSelectedStar: RequestHandler = async (req,res) => {
 
     const { id } = req.body
+    const validatedID = ValidateID(req.body)
+
+    if (validatedID.error){
+        console.log(validatedID.error)
+        const response = {
+            status: StatusCodes.INTERNAL_SERVER_ERROR,
+            message: validatedID.error.details[0].message
+        }
+        res.status(response.status)
+        res.send(response.message)
+    }
     try{
-        const DeletedStar = await prisma.star.delete({
+        const DeletedStar = await prisma.star.update({
             where: {
               id
+            },
+            data: {
+                isDead: true
             }
           })
-
+        res.send(DeletedStar)
         res.status(StatusCodes.OK)
 
     } catch (err) {
