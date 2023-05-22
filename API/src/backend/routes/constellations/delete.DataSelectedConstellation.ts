53311d462530504c906390f10f37632c8821bd32
furtub2 <vladyslav.ishchuk.2@gmail.com>
2023-05-22 20:38:16 +0200

    correct Delete Constellation

--- API/src/backend/routes/constellations/delete.DataSelectedConstellation.ts
+++ API/src/backend/routes/constellations/delete.DataSelectedConstellation.ts
@@ -2,18 +2,35 @@ import { RequestHandler } from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { prisma } from '../../database'
 import { checkPrismaError } from '../../utils/prisma.utils'
+import { ValidateID } from '../../utils/validation.utils'
 
 export const DeleteSelectedConstellation: RequestHandler = async (req,res) => {
 
     const { id } = req.body
+    const validatedID = ValidateID(req.body)
+
+    if (validatedID.error){
+        console.log(validatedID.error)
+        const response = {
+            status: StatusCodes.INTERNAL_SERVER_ERROR,
+            message: validatedID.error.details[0].message
+        }
+        res.status(response.status)
+        res.send(response.message)
+    }
+    else{
     try{
-        const DeletedConstellation = await prisma.star.delete({
+        const DeletedConstellation = await prisma.constellation.update({
             where: {
               id
+            },
+            data: {
+              isDead: true
             }
           })
 
         res.status(StatusCodes.OK)
+        res.send(DeletedConstellation)
 
     } catch (err) {
         console.error(err)
@@ -23,4 +40,5 @@ export const DeleteSelectedConstellation: RequestHandler = async (req,res) => {
         res.status(response.status)
         res.send(response.message)
         }
+  }
 }
