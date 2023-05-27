585bcae4943df42d5ab9f5575ab6aafb23b07c01
Jarek Bigaj <j.bigaj@interia.pl>
2023-05-27 19:50:47 +0200

    styling form

--- night-sky/src/helperComponents/PopupCard.js
+++ night-sky/src/helperComponents/PopupCard.js
@@ -26,7 +26,7 @@ const PopupCard = styled((
                       {
                           Object.entries(formData).map(([key,value])=>{
                               return (
-                                  <div key={key+"div"}>
+                                  <div key={key+"div"} className="row-wrapper">
                                       <label key={key+"label"}>{key + " :"}</label>
                                       {key === "constellationId" ?
                                         <select value={value} name={key} onChange={handleInputChange}>
@@ -41,13 +41,20 @@ const PopupCard = styled((
                                           })}
                                         </select>
                                       :
-                                        <input key={key} type="text" name={key} value={value} onChange={handleInputChange} />
+                                        <input
+                                         key={key} 
+                                         type="text" 
+                                         name={key} 
+                                         value={value} 
+                                         onChange={handleInputChange} />
                                       }
                                   </div>
                               )
                           })
                       }
-                    <button type="submit">Submit</button>
+                    <div className="button-wrapper-small">
+                      <button class="button-neon-styled-small" type="submit">Submit</button>
+                    </div>
                   </form>
                 </div>
               </div>
@@ -68,18 +75,182 @@ position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: rgba(0, 0, 0, 0.5); /* Semi-transparent background overlay */
+  background-color: rgba(0, 0, 0, 0.5);
   z-index: 9999;
 }
 
 .popup-content {
-  background-color: white;
-  padding: 20px;
-  border: 1px solid #ccc;
+  width: min(350px, 100% - 3rem);
+  margin-inline: auto;
+  color:white;
+  font-size:1.2em;
+  position:relative;
+  background-color: black;
+  height: min(300px, 100% - 3rem);
+  padding: 80px 30px 0px 30px;
+  border: 1px solid inherit;
+  border-radius:0.75em;
 }
 
+.button-wrapper-small{
+  position:absolute;
+  display:flex;
+  bottom:2.5em;
+  left:0;
+  right:0;
+  justify-content: center; 
+}
 
+.button-neon-styled-small{
+  font-size:1.4em;
 
+  background-color: var(--clr-bg);
+  display: inline-block;
+  cursor: pointer;
+  text-decoration: none;
+  color: var(--clr-neon);
+  border: var(--clr-neon) 0.125em solid;
+  padding: 0.25em 1em;
+  border-radius: 0.25em;
+
+  text-shadow: 
+    0 0 0.125em hsl(0 0% 100% / 0.3),
+    0 0 0.45em currentColor;
+
+  box-shadow: 
+    inset 0 0 0.5em 0 var(--clr-neon),
+    0 0 0.5em 0 var(--clr-neon) ;
+
+  transition: background-color 100ms linear;
+}
+
+.button-neon-styled-small::before {
+  pointer-events:none;
+  content: "";
+  position:absolute;
+  background: var(--clr-neon);
+  
+  top:120%;
+  left:0;
+  width:100%;
+  height:100%;
+
+  transform: 
+    perspective(4em) 
+    rotateX(20deg)
+    scale(0.8, 0.26);
+
+  filter: blur(1em);
+  opacity: 0.7;
+}
+
+.button-neon-styled-small:after{
+  content:"";
+  position:absolute;
+  top:0;
+  bottom:0;
+  left:0;
+  right:0;
+
+  opacity:0;
+  transition: opacity 100ms linear;
+}
+
+.button-neon-styled-small:hover,
+.button-neon-styled-small:focus{
+  background: var(--clr-neon);
+  color: white;
+  text-shadow: none;
+}
+
+.button-neon-styled-small:hover::before,
+.button-neon-styled-small:focus::before{
+  opacity:1;
+}
+.button-neon-styled-small:hover::after,
+.button-neon-styled-small:focus::after{
+  opacity:1;
+}
+
+.row-wrapper{
+  display:grid;
+  grid-template-columns: 15ch auto;
+  gap:0.5rem;
+  padding: 0.5rem 1rem;
+}
+
+form {
+  
+}
+
+input{
+  background-color:inherit;
+  border:gray solid 2px;
+  border-radius:5px;
+}
+
+.exit {
+  position: absolute;
+  top:0.5em;
+  right:0.5em;
+  font-size:2em;
+  background-color:inherit;
+  color:white;
+  border:none;
+  cursor:pointer;
+  color: var(--clr-neon);
+  text-shadow: 
+    0 0 0.125em hsl(0 0% 100% / 0.3),
+    0 0 0.45em currentColor;
+  transition: background-color 100ms linear;
+}
+
+.exit::before {
+  pointer-events:none;
+  content: "";
+  position:absolute;
+  background: var(--clr-neon);
+  
+  top:30%;
+  left:0;
+  width:100%;
+  height:100%;
+
+  transform: 
+    perspective(2em) 
+    rotateX(20deg)
+    scale(0.8, 0.26);
+
+  filter: blur(1em);
+  opacity: 0.7;
+}
+
+.button-neon-styled-small:after{
+  content:"";
+  position:absolute;
+  top:0;
+  bottom:0;
+  left:0;
+  right:0;
+
+  opacity:0;
+  transition: opacity 100ms linear;
+}
+
+.exit:hover,
+.exit:focus{
+  color: var(--clr-neon);
+  text-shadow: none;
+}
+
+.exit:hover::before,
+.exit:focus::before{
+  opacity:1;
+}
+.exit:hover::after,
+.exit:focus::after{
+  opacity:1;
+}
 `;
 
 
